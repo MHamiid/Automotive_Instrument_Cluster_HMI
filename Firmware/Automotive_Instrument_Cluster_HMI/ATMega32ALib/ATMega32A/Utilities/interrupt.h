bd69d362c091685812a91863206f3a8e2b83cba9
MHamiid <mmh699@outlook.com>
2022-09-14 02:31:00 +0200

    [Lib] Create interrupt.h

---
+++ Firmware/Automotive_Instrument_Cluster_HMI/ATMega32ALib/ATMega32A/Utilities/interrupt.h
@@ -0,0 +1,37 @@
+/*
+ * interrupt.h
+ *
+ * Created: 8/2/2022 2:55:07 PM
+ *  Author: MHamiid
+ */ 
+
+
+#ifndef INTERRUPT_H_
+#define INTERRUPT_H_
+
+/************************************************************************/
+/* Interrupt Vectors                                                    */
+/************************************************************************/
+
+/* External Interrupt Vectors */
+// External interrupt request 0
+#define EXT_INT_0 __vector_1
+// External interrupt request 1
+#define EXT_INT_1 __vector_2
+// External interrupt request 2
+#define EXT_INT_2 __vector_3
+
+
+
+
+// Set global interrupts -> Sets the I-bit (7th bit) in the status register to 1
+#define sei() __asm__ __volatile__ ("sei" ::: "memory")
+
+// Clear global interrupts -> Sets the I-bit (7th bit) in the status register to 0
+#define cli() __asm__ __volatile__ ("cli" ::: "memory")
+
+/* ISR Function Macro */
+#define ISR(INT_VECT) void INT_VECT(void) __attribute__ ((signal, used));  /*The function prototype (declaration)*/\
+void INT_VECT(void) /*The function body (definition)*/
+
+#endif /* INTERRUPT_H_ */
