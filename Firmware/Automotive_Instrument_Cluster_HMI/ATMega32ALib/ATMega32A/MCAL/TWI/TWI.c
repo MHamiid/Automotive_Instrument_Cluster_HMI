de191fd93c58f50c0b4bbcb6db6465e5d0b7b5c6
MHamiid <mmh699@outlook.com>
2022-09-19 04:19:51 +0200

    [Lib] Add TWI driver

---
+++ Firmware/Automotive_Instrument_Cluster_HMI/ATMega32ALib/ATMega32A/MCAL/TWI/TWI.c
@@ -0,0 +1,348 @@
+/*
+ * TWI.c
+ *
+ * Created: 9/14/2022 9:47:34 PM
+ *  Author: MHamiid
+ */ 
+
+#include "TWI.h"
+#include "../../Utilities/registers.h"
+#include <math.h>
+
+/**
+ * Calculate the value for the TWSR register from the SCL frequency, F_CPU, and the TWI pre-scaler(TWPS0, TWPS1)
+*/
+#define TWBR_FROM_SCL_FREQUENCY(SCL_FREQUENCY, TWSR) ((F_CPU / SCL_FREQUENCY) - 16) / (2 * pow(4, (TWSR & ((1<<TWPS0) | (1<<TWPS1)))))
+
+void TWI_master_init(uint32_t SCLFrequency)
+{
+	// Prevent exceeding the maximum SCL frequency
+	if(SCLFrequency > 400000)
+	{
+		SCLFrequency = 400000;
+	}
+	
+	// Clear the TWI status bits and set the TWI pre-scaler to 1
+	TWSR = 0x00;
+	
+	// Calculate TWBR value from SCL frequency and TWI pre-scaler value
+	TWBR  = TWBR_FROM_SCL_FREQUENCY(SCLFrequency, TWSR);
+}
+
+void TWI_slave_init(uint8_t slaveAddress)
+{
+	// Clear the TWI status bits
+	TWSR &= ~(TWI_TWSR_STATUS_BITS_MASK);
+	
+	// Set the slave address in TWI slave register
+	TWAR = slaveAddress;
+}
+
+EN_TWI_EVENT_STATUS_t TWI_master_start()
+{
+	// Clear TWINT flag to start a new event operation | Generate start condition event | Enable TWI
+	TWCR |= (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
+	
+	// Wait until TWI finish its current job/event
+	while(!(TWCR & (1<<TWINT)));		// Busy wait
+	
+	// Check the TWI status
+	if((TWSR & TWI_TWSR_STATUS_BITS_MASK) == TWI_START_SENT_STATE)
+	{
+		return TWI_START_SENT;
+	}
+	else
+	{
+		return TWI_UNHANDLED_EVENT;
+	}
+}
+
+EN_TWI_EVENT_STATUS_t TWI_master_repeatedStart()
+{
+	// Clear TWINT flag to start a new event operation | Generate start condition event | Enable TWI
+	TWCR |= (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
+	
+	// Wait until TWI finish its current job/event
+	while(!(TWCR & (1<<TWINT)));		// Busy wait
+	
+	// Check the TWI status
+	if((TWSR & TWI_TWSR_STATUS_BITS_MASK) == TWI_REPEATED_START_SENT_STATE)
+	{
+		return TWI_REPEATED_START_SENT;
+	}
+	else
+	{
+		return TWI_UNHANDLED_EVENT;
+	}
+	
+}
+
+EN_TWI_EVENT_STATUS_t TWI_master_transmit_address(uint8_t slaveAddress, uint8_t rw)
+{
+	uint8_t TWI_status = 0;
+	
+	// Write the 7-bits slave address + R/W bit in TWI data register - data **MUST** be written to TWDR before clearing TWINT
+	if(rw == TWI_WRITE_BIT)
+	{
+		TWDR = slaveAddress;		// Write slave address + write(0) in TWI data register
+	}
+	else if(rw == TWI_READ_BIT)
+	{
+		TWDR = slaveAddress + 1;	// Write slave address + read(1) in TWI data register
+	}
+	else
+	{
+		// Invalid Read/Write bit
+		return TWI_INVALID_OPERATION;
+	}
+	
+	// Clear TWINT flag to start a new event operation | Enable TWI
+	TWCR |= (1<<TWINT) | (1<<TWEN);
+	
+	// Wait until TWI finish its current job/event
+	while(!(TWCR & (1<<TWINT)));		// Busy wait
+	
+	// Read the TWI status
+	TWI_status = (TWSR & TWI_TWSR_STATUS_BITS_MASK);
+	
+	if(TWI_status == TWI_SLAVE_ADDRESS_W_SENT_ACK_RECEIVED_STATE)
+	{
+		return TWI_SLAVE_ADDRESS_W_SENT_ACK_RECEIVED;
+	}
+	else if(TWI_status == TWI_SLAVE_ADDRESS_W_SENT_NACK_RECEIVED_STATE)
+	{
+		return TWI_SLAVE_ADDRESS_W_SENT_NACK_RECEIVED;
+	}
+	else if(TWI_status == TWI_SLAVE_ADDRESS_R_SENT_ACK_RECEIVED_STATE)
+	{
+		return TWI_SLAVE_ADDRESS_R_SENT_ACK_RECEIVED;
+	}
+	else if(TWI_status == TWI_SLAVE_ADDRESS_R_SENT_NACK_RECEIVED_STATE)
+	{
+		return TWI_SLAVE_ADDRESS_R_SENT_NACK_RECEIVED;
+	}
+	else
+	{
+		return TWI_UNHANDLED_EVENT;
+	}
+	
+}
+
+EN_TWI_EVENT_STATUS_t TWI_master_transmit(uint8_t data)
+{
+	uint8_t TWI_status = 0;
+
+	// Set the data into TWI data register - data **MUST** be written to TWDR before clearing TWINT
+	TWDR = data;
+	
+	// Clear TWINT flag to start a new event operation | Enable TWI
+	TWCR |= (1<<TWINT) | (1<<TWEN);
+	
+	// Wait until TWI finish its current job/event
+	while(!(TWCR&(1<<TWINT)));		// Busy wait
+	
+	// Read the TWI status
+	TWI_status = (TWSR & TWI_TWSR_STATUS_BITS_MASK);
+	
+	// Check data transmission status
+	if(TWI_status == TWI_MASTER_DATA_SENT_ACK_RECEIVED_STATE)
+	{
+		return TWI_MASTER_DATA_SENT_ACK_RECEIVED;
+	}
+	else if(TWI_status == TWI_MASTER_DATA_SENT_NACK_RECEIVED_STATE)
+	{
+		return TWI_MASTER_DATA_SENT_NACK_RECEIVED;
+	}
+	else
+	{
+		return TWI_UNHANDLED_EVENT;
+	}
+}
+
+EN_TWI_EVENT_STATUS_t TWI_master_receive(uint8_t* receivedData, uint8_t response)
+{
+	uint8_t TWI_status = 0;
+
+	if(response == TWI_ACK)
+	{
+		// Clear TWINT flag to start a new event operation | Enable TWI | Enable Generation of ACK when data is received
+		TWCR |= (1<<TWINT) | (1<<TWEN) | (1<<TWEA);
+	}
+	else if(response == TWI_NACK)
+	{
+		// Clear TWINT flag to start a new event operation | Enable TWI
+		TWCR |= (1<<TWINT) | (1<<TWEN);
+		// Enable Generation of NACK when data is received
+		TWCR &= ~(1<<TWEA);
+	}
+	else
+	{
+		// Invalid ACK status
+		return TWI_INVALID_OPERATION;
+	}
+	
+	// Wait until TWI finish its current job/event
+	while(!(TWCR & (1<<TWINT)));		// Busy wait
+	
+	// Read the data from TWI data register
+	*receivedData = TWDR;
+	
+	// Read the TWI status
+	TWI_status = (TWSR & TWI_TWSR_STATUS_BITS_MASK);
+	
+	// Check data reception and ACK/NACK sent status
+	if(TWI_status == TWI_MASTER_DATA_RECEIVED_ACK_SENT_STATE)
+	{
+		return TWI_MASTER_DATA_RECEIVED_ACK_SENT;			// Data is received and ACK is sent
+	}
+	else if(TWI_status == TWI_MASTER_DATA_RECEIVED_NACK_SENT_STATE)
+	{
+		return TWI_MASTER_DATA_RECEIVED_NACK_SENT;			// Data is received
+	}
+	else
+	{
+		return TWI_UNHANDLED_EVENT;
+	}
+}
+
+void TWI_master_stop()
+{
+	// Clear TWINT flag to start a new event operation | Generate stop condition event | Enable TWI
+	TWCR |= (1<<TWINT) | (1<<TWSTO) | (1<<TWEN);
+	
+	// Wait until stop condition execution (TWSTO is cleared after execution)
+	while(TWCR & (1<<TWSTO));		// Busy wait
+}
+
+EN_TWI_EVENT_STATUS_t TWI_slave_listen()
+{
+	uint8_t TWI_status = 0;
+
+	// Clear TWINT flag to start a new event operation | Enable ACK generation for this device slave address of to the general call address (if enabled) | Enable TWI
+	TWCR |= (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
+	
+	// Make sure that TWSTA and TWSTO are zeros
+	TWCR &= ~((1<<TWSTA) | (1<<TWSTO));
+	
+	// Wait until the device is being addressed
+	while(!(TWCR & (1<<TWINT)));		// Busy wait
+	
+	// Read the TWI status
+	TWI_status = (TWSR & TWI_TWSR_STATUS_BITS_MASK);
+	
+	// Slave address received + write operation, and ACK returned
+	if(TWI_status == TWI_SLAVE_ADDRESS_W_RECEIVED_STATE)
+	{
+		return TWI_SLAVE_ADDRESS_W_RECEIVED;
+	}
+	// Slave address received + read operation, and ACK returned
+	else if(TWI_status == TWI_SLAVE_ADDRESS_R_RECEIVED_STATE)
+	{
+		return TWI_SLAVE_ADDRESS_R_RECEIVED;
+	}
+	// General call address received and ACK returned
+	else if(TWI_status == TWI_GENERAL_ADDRESS_RECEIVED_STATE)
+	{
+		return TWI_GENERAL_ADDRESS_RECEIVED;
+	}
+	else
+	{
+		return TWI_UNHANDLED_EVENT;
+	}
+	
+}
+
+EN_TWI_EVENT_STATUS_t TWI_slave_receive(uint8_t* receivedData, uint8_t response)
+{
+	uint8_t TWI_status = 0;
+	
+	if(response == TWI_ACK)
+	{
+		// Clear TWINT flag to start a new event operation | Enable ACK generation for this device slave address of to the general call address (if enabled) | Enable TWI
+		TWCR |= (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
+	}
+	else if(response == TWI_NACK)
+	{
+		// Clear TWINT flag to start a new event operation | Enable TWI
+		TWCR |= (1<<TWINT) | (1<<TWEN);
+		// Enable NACK generation for this device slave address of to the general call address (if enabled)
+		TWCR &= ~(1<<TWEA);
+	}
+	else
+	{
+		// Invalid ACK status
+		return TWI_INVALID_OPERATION;
+	}
+	
+	// Make sure that TWSTA and TWSTO are zeros
+	TWCR &= ~((1<<TWSTA) | (1<<TWSTO));
+	
+	// Wait until data is received
+	while(!(TWCR & (1<<TWINT)));		// Busy wait
+	
+	// Read the TWI status
+	TWI_status = (TWSR & TWI_TWSR_STATUS_BITS_MASK);
+	
+	// Read the received data from TWI data register
+	*receivedData = TWDR;
+	
+	// Data received, and ACK returned
+	if(TWI_status == TWI_SLAVE_DATA_RECEIVED_ACK_SENT_STATE)
+	{
+		return TWI_SLAVE_DATA_RECEIVED_ACK_SENT;
+	}
+	// Data received, and NACK returned
+	else if(TWI_status == TWI_SLAVE_DATA_RECEIVED_NACK_SENT_STATE)
+	{	
+		return TWI_SLAVE_DATA_RECEIVED_NACK_SENT;
+	}
+	// STOP or REPEATED START received
+	else if(TWI_status == TWI_SLAVE_STO_RSTA_RECEIVED_STATE)
+	{
+		return TWI_SLAVE_STO_RSTA_RECEIVED;
+	}
+	else
+	{
+		return TWI_UNHANDLED_EVENT;
+	}
+}
+
+EN_TWI_EVENT_STATUS_t TWI_slave_transmit(uint8_t data)
+{
+	uint8_t TWI_status = 0;
+
+	// Set the data into TWI data register - data **MUST** be written to TWDR before clearing TWINT
+	TWDR = data;
+
+	// Clear TWINT flag to start a new event operation | Enable ACK generation for this device slave address of to the general call address (if enabled) | Enable TWI
+	TWCR |= (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
+	
+	// Make sure that TWSTA and TWSTO are zeros
+	TWCR &= ~((1<<TWSTA) | (1<<TWSTO));
+	
+	// Wait until data is transmitted
+	while(!(TWCR & (1<<TWINT)));		// Busy wait
+	
+	// Read the TWI status
+	TWI_status = (TWSR & TWI_TWSR_STATUS_BITS_MASK);
+	
+	// Data sent, and ACK returned
+	if(TWI_status == TWI_SLAVE_DATA_SENT_ACK_RECEIVED_STATE)
+	{
+		return TWI_SLAVE_DATA_SENT_ACK_RECEIVED;
+	}
+	// Data sent, and NACK returned
+	else if(TWI_status == TWI_SLAVE_DATA_SENT_NACK_RECEIVED_STATE)
+	{	
+		return TWI_SLAVE_DATA_SENT_NACK_RECEIVED;
+	}
+	// STOP or REPEATED START received
+	else if(TWI_status == TWI_SLAVE_STO_RSTA_RECEIVED_STATE)
+	{
+		return TWI_SLAVE_STO_RSTA_RECEIVED;
+	}
+	else
+	{
+		return TWI_UNHANDLED_EVENT;
+	}
+}
---
+++ Firmware/Automotive_Instrument_Cluster_HMI/ATMega32ALib/ATMega32A/MCAL/TWI/TWI.h
@@ -0,0 +1,232 @@
+/*
+ * TWI.h
+ *
+ * Created: 9/14/2022 9:47:23 PM
+ *  Author: MHamiid
+ */ 
+
+
+#ifndef TWI_H_
+#define TWI_H_
+
+#ifndef F_CPU
+/* prevent compiler error by supplying a default F_CPU */
+# warning "F_CPU not defined for <ATMega32A/MCAL/TWI/TWI.h>. Using 1 MHz as a default F_CPU"
+
+/**
+  * The value 1 MHz only provided as a fall back 
+  * if no user-provided definition could be found
+*/
+# define F_CPU 1000000UL
+#endif
+
+#include <stdint.h>
+
+// Mask for TWSR status 5-bits, the higher 5-bits
+#define TWI_TWSR_STATUS_BITS_MASK 0xF8
+
+#define TWI_WRITE_BIT				0
+#define TWI_READ_BIT				1
+#define TWI_ACK						0
+#define TWI_NACK					1
+
+
+/* TWSR Register States */
+/* Master States */
+#define TWI_START_SENT_STATE							0x08
+#define TWI_REPEATED_START_SENT_STATE					0x10
+#define TWI_SLAVE_ADDRESS_W_SENT_ACK_RECEIVED_STATE		0x18
+#define TWI_SLAVE_ADDRESS_W_SENT_NACK_RECEIVED_STATE	0x20
+#define TWI_SLAVE_ADDRESS_R_SENT_ACK_RECEIVED_STATE		0x40
+#define TWI_SLAVE_ADDRESS_R_SENT_NACK_RECEIVED_STATE	0x48
+#define TWI_MASTER_DATA_SENT_ACK_RECEIVED_STATE			0x28
+#define TWI_MASTER_DATA_SENT_NACK_RECEIVED_STATE		0x30
+#define TWI_MASTER_DATA_RECEIVED_ACK_SENT_STATE			0x50
+#define TWI_MASTER_DATA_RECEIVED_NACK_SENT_STATE		0x58
+#define TWI_MASTER_ARBITRATION_LOST_STATE				0x38
+/* Slave States */
+#define TWI_SLAVE_ADDRESS_W_RECEIVED_STATE				0x60
+#define TWI_SLAVE_ADDRESS_R_RECEIVED_STATE				0xA8
+#define TWI_GENERAL_ADDRESS_RECEIVED_STATE				0x70
+#define TWI_SLAVE_DATA_RECEIVED_ACK_SENT_STATE			0x80
+#define TWI_SLAVE_DATA_RECEIVED_NACK_SENT_STATE			0x88
+#define TWI_SLAVE_DATA_SENT_ACK_RECEIVED_STATE			0xB8
+#define TWI_SLAVE_DATA_SENT_NACK_RECEIVED_STATE			0xC0
+#define TWI_SLAVE_STO_RSTA_RECEIVED_STATE				0xA0
+
+
+typedef enum EN_TWI_EVENT_STATUS_t
+{
+	TWI_INVALID_OPERATION,
+	TWI_UNHANDLED_EVENT,
+	/* Master Events */
+	TWI_START_SENT,
+	TWI_REPEATED_START_SENT,
+	TWI_SLAVE_ADDRESS_W_SENT_ACK_RECEIVED,
+	TWI_SLAVE_ADDRESS_W_SENT_NACK_RECEIVED,
+	TWI_SLAVE_ADDRESS_R_SENT_ACK_RECEIVED,
+	TWI_SLAVE_ADDRESS_R_SENT_NACK_RECEIVED,
+	TWI_MASTER_DATA_SENT_ACK_RECEIVED,
+	TWI_MASTER_DATA_SENT_NACK_RECEIVED,
+	TWI_MASTER_DATA_RECEIVED_ACK_SENT,
+	TWI_MASTER_DATA_RECEIVED_NACK_SENT,
+	TWI_MASTER_ARBITRATION_LOST,
+	/* Slave Events */
+	TWI_SLAVE_ADDRESS_W_RECEIVED,
+	TWI_SLAVE_ADDRESS_R_RECEIVED,
+	TWI_GENERAL_ADDRESS_RECEIVED,
+	TWI_SLAVE_DATA_RECEIVED_ACK_SENT,
+	TWI_SLAVE_DATA_RECEIVED_NACK_SENT,
+	TWI_SLAVE_DATA_SENT_ACK_RECEIVED,
+	TWI_SLAVE_DATA_SENT_NACK_RECEIVED,
+	TWI_SLAVE_STO_RSTA_RECEIVED
+	
+} EN_TWI_EVENT_STATUS_t;
+
+
+/**
+ * @brief Initialize TWI in master mode
+ * 
+ * @param SCLFrequency											SCL line clock frequency (Max value: 400000 "400 KHz")
+ *
+ * @return void
+ */
+void TWI_master_init(uint32_t SCLFrequency);
+
+/**
+ * @brief Transmit a START condition as soon as the bus becomes free
+ *
+ * Function is called after TWI has been initialized in master mode
+ * 
+ * @return TWI_START_SENT										START condition has been transmitted
+ * @return TWI_UNHANDLED_EVENT									Received unexpected status code
+ */
+EN_TWI_EVENT_STATUS_t TWI_master_start();
+
+/**
+ * @brief Transmit a REPEATED START condition
+ *
+ * Function is called after TWI has been initialized in master mode
+ * 
+ * REPEATED START enables the master to switch between slaves,
+ * master transmitter mode and master receiver mode without losing control of the bus
+ *
+ * @return TWI_REPEATED_START_SENT								REPEATED START condition has been transmitted
+ * @return TWI_UNHANDLED_EVENT									Received unexpected status code
+ */
+EN_TWI_EVENT_STATUS_t TWI_master_repeatedStart();
+
+/**
+ * @brief Transmit a STOP condition
+ * 
+ * STOP releases the bus and ends the communication 
+ *
+ * @return void
+ */
+void TWI_master_stop();
+
+/**
+ * @brief Transmit the 7-bit address of the slave to start communication with, and the read/write bit
+ * 
+ * Function is called after a START/REPEATED START condition has been sent by the master.
+ * Master transmitter mode can be entered if write bit is selected, otherwise the master receiver mode can be entered if read bit is selected
+ *
+ * @param slaveAddress											Slave's 7-bit address that the master wants to start communication with
+ * @param rw													TWI_WRITE_BIT for master transmit operation. TWI_READ_BIT for master receive operation
+ *
+ * @return TWI_SLAVE_ADDRESS_W_SENT_ACK_RECEIVED				Slave address + Write sent and an ACK received
+ * @return TWI_SLAVE_ADDRESS_W_SENT_NACK_RECEIVED				Slave address + Write sent and a NACK received
+ * @return TWI_SLAVE_ADDRESS_R_SENT_ACK_RECEIVED				Slave address + Read sent and an ACK received
+ * @return TWI_SLAVE_ADDRESS_R_SENT_NACK_RECEIVED				Slave address + Read sent and a NACK received
+ * @return TWI_UNHANDLED_EVENT									Received unexpected status code
+ */
+EN_TWI_EVENT_STATUS_t TWI_master_transmit_address(uint8_t slaveAddress, uint8_t rw);
+
+/**
+ * @brief Transmit a byte of data to the addressed slave after the master has entered master transmitter mode
+ * 
+ * Function is called after the slave address + write has been sent by the master (Master transmitter mode entered).
+ *
+ * @param data													A byte of data to be transmitted to addressed slave
+ *
+ * @return TWI_MASTER_DATA_SENT_ACK_RECEIVED					Data sent and an ACK received
+ * @return TWI_MASTER_DATA_SENT_NACK_RECEIVED					Data sent and a NACK received
+ * @return TWI_UNHANDLED_EVENT									Received unexpected status code
+ */
+EN_TWI_EVENT_STATUS_t TWI_master_transmit(uint8_t data);
+
+/**
+ * @brief Receive a byte of data from the addressed slave after the master has entered master receiver mode
+ * 
+ * Function is called after the slave address + read has been sent by the master (Master receiver mode entered).
+ * Master should inform the transmitting slave by sending a TWI_NACK as a response after the last received data byte,
+ * the transfer is then ended by generating a STOP condition or a REPEATED START condition
+ *
+ * @param receivedData											A byte of data received from addressed slave
+ * @param response												Master response to the received data. Can be [TWI_ACK, TWI_NACK]
+ *
+ * @return TWI_INVALID_OPERATION								Invalid @param response used
+ * @return TWI_MASTER_DATA_RECEIVED_ACK_SENT					Data received and an ACK sent
+ * @return TWI_MASTER_DATA_RECEIVED_NACK_SENT					Data received and an NACK sent
+ * @return TWI_UNHANDLED_EVENT									Received unexpected status code
+ */
+EN_TWI_EVENT_STATUS_t TWI_master_receive(uint8_t* receivedData, uint8_t response);
+
+/**
+ * @brief Initialize TWI in slave mode
+ * 
+ * @param slaveAddress											Device's own 7-bit slave address, with will be used buy other masters to address this device
+ *
+ * @return void
+ */
+void TWI_slave_init(uint8_t slaveAddress);
+
+/**
+ * @brief Slave listens to the bus for its own slave address
+ * 
+ * Function is called after TWI has been initialized in slave mode
+ * Slave receiver mode is entered if own slave address + write is received, otherwise slave transmitter mode is entered if own slave address + read is received
+ *
+ * @return TWI_SLAVE_ADDRESS_W_RECEIVED							Own slave address + Write received and an ACK sent
+ * @return TWI_SLAVE_ADDRESS_R_RECEIVED							Own slave address + Read received and an ACK sent
+ * @return TWI_GENERAL_ADDRESS_RECEIVED							General call address received and an ACK sent
+ * @return TWI_UNHANDLED_EVENT									Received unexpected status code
+ */
+EN_TWI_EVENT_STATUS_t TWI_slave_listen();
+
+/**
+ * @brief Receive a byte of data from the master after the slave has entered slave receiver mode
+ * 
+ * Function is called after own slave address + write has been received from the master (Slave receiver mode entered).
+ * Slave receiver can send TWI_NACK to be used as a response to indicate that the slave is not able to receive any more bytes from the master,
+ * the transfer is then ended by receiving a STOP condition or a REPEATED START condition
+ *
+ * @param receivedData											A byte of data received from master
+ * @param response												Slave response to the received data. Can be [TWI_ACK, TWI_NACK]
+ *
+ * @return TWI_INVALID_OPERATION								Invalid @param response used
+ * @return TWI_SLAVE_DATA_RECEIVED_ACK_SENT						Data received and an ACK sent
+ * @return TWI_SLAVE_DATA_RECEIVED_NACK_SENT					Data received and an NACK sent
+ * @return TWI_SLAVE_STO_RSTA_RECEIVED							STOP or REPEATED START condition received
+ * @return TWI_UNHANDLED_EVENT									Received unexpected status code
+ */
+EN_TWI_EVENT_STATUS_t TWI_slave_receive(uint8_t* receivedData, uint8_t response);
+
+/**
+ * @brief Transmit a byte of data to the master after the slave has entered slave transmitter mode
+ * 
+ * Function is called after own slave address + read has been received from the master (Slave transmitter mode entered).
+ * Slave transmitter would receive a NACK from the master to indicate that the master has received the final byte from the slave,
+ * the transfer is then ended by receiving a STOP condition or a REPEATED START condition
+ *
+ * @param data													A byte of data to be transmitted to the master
+ *
+ * @return TWI_SLAVE_DATA_SENT_ACK_RECEIVED						Data sent and an ACK received
+ * @return TWI_SLAVE_DATA_SENT_NACK_RECEIVED					Data sent and an NACK received
+ * @return TWI_SLAVE_STO_RSTA_RECEIVED							STOP or REPEATED START condition received
+ * @return TWI_UNHANDLED_EVENT									Received unexpected status code
+ */
+EN_TWI_EVENT_STATUS_t TWI_slave_transmit(uint8_t data);
+
+
+#endif /* TWI_H_ */
