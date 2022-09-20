e65bb1d1950b11e239a2d53bee3ce7ae50eefeda
MHamiid <mmh699@outlook.com>
2022-09-21 00:13:53 +0200

    [Lib] Add DIO driver

---
+++ Firmware/Automotive_Instrument_Cluster_HMI/ATMega32ALib/ATMega32A/MCAL/DIO/DIO.c
@@ -0,0 +1,427 @@
+/*
+ * DIO.c
+ *
+ * Created: 9/20/2022 9:09:55 PM
+ *  Author: MHamiid
+ */ 
+
+#include "DIO.h"
+#include "../../Utilities/registers.h"
+#include "../../Utilities/interrupt.h"
+
+/* External Interrupts Callback Functions */
+static void(*INT0_CALLBACK_FUNCTION)() = 0;
+static void(*INT1_CALLBACK_FUNCTION)() = 0;
+static void(*INT2_CALLBACK_FUNCTION)() = 0;
+
+EN_DIOErrorStatus_t DIO_init(EN_DIOPort_t port, EN_DIOPin_t pinNumber, EN_DIODirection_t direction)
+{
+	// Validate the pin number
+	if(!(pinNumber >= DIO_PIN_0 && pinNumber <= DIO_PIN_7))
+	{
+		return DIO_INVALID_PIN_NUMBER;
+	}
+	
+	switch(port)
+	{
+		case DIO_PORT_A:
+			if(direction == DIO_DIRECTION_INPUT)
+			{
+				BIT_CLEAR(DDRA, pinNumber);    // Set pin direction to input
+				return DIO_NO_ERROR;
+			}
+			else if(direction == DIO_DIRECTION_OUTPUT)
+			{
+				BIT_SET(DDRA, pinNumber);      // Set pin direction to output
+				return DIO_NO_ERROR;
+			}
+			else
+			{
+				// Direction error handling
+				return DIO_INVALID_DIRECTION;
+			}
+		
+		case DIO_PORT_B:
+			if(direction == DIO_DIRECTION_INPUT)
+			{
+				BIT_CLEAR(DDRB, pinNumber);    // Set pin direction to input
+				return DIO_NO_ERROR;
+			}
+			else if(direction == DIO_DIRECTION_OUTPUT)
+			{
+				BIT_SET(DDRB, pinNumber);      // Set pin direction to output
+				return DIO_NO_ERROR;
+			}
+			else
+			{
+				// Direction error handling
+				return DIO_INVALID_DIRECTION;
+			}
+		
+		case DIO_PORT_C:
+			if(direction == DIO_DIRECTION_INPUT)
+			{
+				BIT_CLEAR(DDRC, pinNumber);    // Set pin direction to input
+				return DIO_NO_ERROR;
+			}
+			else if(direction == DIO_DIRECTION_OUTPUT)
+			{
+				BIT_SET(DDRC, pinNumber);      // Set pin direction to output
+				return DIO_NO_ERROR;
+			}
+			else
+			{
+				// direction Error handling
+				return DIO_INVALID_DIRECTION;
+			}
+		
+		case DIO_PORT_D:
+			if(direction == DIO_DIRECTION_INPUT)
+			{
+				BIT_CLEAR(DDRD, pinNumber);    // Set pin direction to input
+				return DIO_NO_ERROR;
+			}
+			else if(direction == DIO_DIRECTION_OUTPUT)
+			{
+				BIT_SET(DDRD, pinNumber);      // Set pin direction to output
+				return DIO_NO_ERROR;
+			}
+			else
+			{
+				// Direction error handling
+				return DIO_INVALID_DIRECTION;
+			}
+		
+		default:
+			// Port Number error handling
+			return DIO_INVALID_PORT;
+	}
+}
+
+EN_DIOErrorStatus_t DIO_write(EN_DIOPort_t port, EN_DIOPin_t pinNumber, EN_DIODigitalValue_t digitalValue)
+{
+	// Validate the pin number
+	if(!(pinNumber >= DIO_PIN_0 && pinNumber <= DIO_PIN_7))
+	{
+		return DIO_INVALID_PIN_NUMBER;
+	}
+	
+	switch(port)
+	{
+		case DIO_PORT_A:
+			if(digitalValue == DIO_LOW)
+			{
+				BIT_CLEAR(PORTA, pinNumber);    // Write 0 to the pin output
+				return DIO_NO_ERROR;
+			}
+			else if(digitalValue == DIO_HIGH)
+			{
+				BIT_SET(PORTA, pinNumber);      // Write 1 to the pin output
+				return DIO_NO_ERROR;
+			}
+			else
+			{
+				// Digital output value error handling
+				return DIO_INVALID_DIGITAL_VALUE;
+			}
+		
+		case DIO_PORT_B:
+			if(digitalValue == DIO_LOW)
+			{
+				BIT_CLEAR(PORTB, pinNumber);    // Write 0 to the pin output
+				return DIO_NO_ERROR;
+			}
+			else if(digitalValue == DIO_HIGH)
+			{
+				BIT_SET(PORTB, pinNumber);      // Write 1 to the pin output
+				return DIO_NO_ERROR;
+			}
+			else
+			{
+				// Digital value error handling
+				return DIO_INVALID_DIGITAL_VALUE;
+			}
+		
+		case DIO_PORT_C:
+			if(digitalValue == DIO_LOW)
+			{
+				BIT_CLEAR(PORTC, pinNumber);    // Write 0 to the pin output
+				return DIO_NO_ERROR;
+			}
+			else if(digitalValue == DIO_HIGH)
+			{
+				BIT_SET(PORTC, pinNumber);      // Write 1 to the pin output
+				return DIO_NO_ERROR;
+			}
+			else
+			{
+				// Digital output value error handling
+				return DIO_INVALID_DIGITAL_VALUE;
+			}
+		
+		case DIO_PORT_D:
+			if(digitalValue == DIO_LOW)
+			{
+				BIT_CLEAR(PORTD, pinNumber);    // Write 0 to the pin output
+				return DIO_NO_ERROR;
+			}
+			else if(digitalValue == DIO_HIGH)
+			{
+				BIT_SET(PORTD, pinNumber);      // Write 1 to the pin output
+				return DIO_NO_ERROR;
+			}
+			else
+			{
+				// Digital output value error handling
+				return DIO_INVALID_DIGITAL_VALUE;
+			}
+		
+		default:
+			// Port Number error handling
+			return DIO_INVALID_PORT;
+	}
+}
+
+EN_DIOErrorStatus_t DIO_toggle(EN_DIOPort_t port, EN_DIOPin_t pinNumber)
+{
+	// Validate the pin number
+	if(!(pinNumber >= DIO_PIN_0 && pinNumber <= DIO_PIN_7))
+	{
+		return DIO_INVALID_PIN_NUMBER;
+	}
+	
+	switch(port)
+	{
+		case DIO_PORT_A:
+			BIT_TOGGLE(PORTA, pinNumber);     // Toggle the current pin state
+			return DIO_NO_ERROR;
+		
+		case DIO_PORT_B:
+			BIT_TOGGLE(PORTB, pinNumber);     // Toggle the current pin state
+			return DIO_NO_ERROR;
+		
+		case DIO_PORT_C:
+			BIT_TOGGLE(PORTC, pinNumber);     // Toggle the current pin state
+			return DIO_NO_ERROR;
+		
+		case DIO_PORT_D:
+			BIT_TOGGLE(PORTD, pinNumber);     // Toggle the current pin state
+			return DIO_NO_ERROR;
+		
+		default:
+			// Port Number error handling
+			return DIO_INVALID_PORT;
+	}
+}
+
+EN_DIOErrorStatus_t DIO_read(EN_DIOPort_t port, EN_DIOPin_t pinNumber, EN_DIODigitalValue_t* retDigitalValue)
+{
+	// Validate the pin number
+	if(!(pinNumber >= DIO_PIN_0 && pinNumber <= DIO_PIN_7))
+	{
+		return DIO_INVALID_PIN_NUMBER;
+	}
+	
+	switch(port)
+	{
+		case DIO_PORT_A:
+			*retDigitalValue = BIT_READ(PINA, pinNumber);		// Read pin bit ===> 0 (DIO_LOW) or 1 (DIO_HIGH)
+			return DIO_NO_ERROR;
+		
+		case DIO_PORT_B:
+			*retDigitalValue = BIT_READ(PINB, pinNumber);		// Read pin bit ===> 0 (DIO_LOW) or 1 (DIO_HIGH)
+			return DIO_NO_ERROR;
+		
+		case DIO_PORT_C:
+			*retDigitalValue = BIT_READ(PINC, pinNumber);		// Read pin bit ===> 0 (DIO_LOW) or 1 (DIO_HIGH)
+			return DIO_NO_ERROR;
+		
+		case DIO_PORT_D:
+			*retDigitalValue = BIT_READ(PIND, pinNumber);		// Read pin bit ===> 0 (DIO_LOW) or 1 (DIO_HIGH)
+			return DIO_NO_ERROR;
+		
+		default:
+			// Port Number error handling
+			return DIO_INVALID_PORT;
+	}
+}
+
+/*
+* Currently this function is just limited to sense interrupts on rising edge for simplicity
+*/
+EN_DIOErrorStatus_t DIO_enableExternalInterrupt(EN_DIOExternalInterrupt_t externalInterruptType, EN_DIOExternalInterruptSense_t externalInterruptSense)
+{
+	switch(externalInterruptType)
+	{
+		case DIO_INT0:
+			/* Initialize External Interrupts for INT0 */
+		
+			// Enable global interrupts
+			sei();
+		
+			/* Set The External Interrupt Sense */
+			if(externalInterruptSense == DIO_EXTERNAL_INT_LOW_LEVEL)
+			{
+				BIT_CLEAR(MCUCR, ISC00);
+				BIT_CLEAR(MCUCR, ISC01);
+			}
+			else if(externalInterruptSense == DIO_EXTERNAL_INT_ANY_LOGICAL_CHANGE)
+			{
+				BIT_SET(MCUCR, ISC00);
+				BIT_CLEAR(MCUCR, ISC01);
+			}
+			else if(externalInterruptSense == DIO_EXTERNAL_INT_FALLING_EDGE)
+			{
+				BIT_CLEAR(MCUCR, ISC00);
+				BIT_SET(MCUCR, ISC01);
+			}
+			else if(externalInterruptSense == DIO_EXTERNAL_INT_RISING_EDGE)
+			{
+				BIT_SET(MCUCR, ISC00);
+				BIT_SET(MCUCR, ISC01);
+			}
+			else
+			{
+				return DIO_INVALID_EXTERNAL_INTERRUPT_SENSE;
+			}
+			
+			// Enable external interrupt 0 - INT0
+			BIT_SET(GICR, INT0);
+		
+			return DIO_NO_ERROR;
+		
+		case DIO_INT1:
+			/* Initialize External Interrupts for INT1 */
+
+			// Enable global interrupts
+			sei();
+		
+			/* Set The External Interrupt Sense */
+			if(externalInterruptSense == DIO_EXTERNAL_INT_LOW_LEVEL)
+			{
+				BIT_CLEAR(MCUCR, ISC10);
+				BIT_CLEAR(MCUCR, ISC11);
+			}
+			else if(externalInterruptSense == DIO_EXTERNAL_INT_ANY_LOGICAL_CHANGE)
+			{
+				BIT_SET(MCUCR, ISC10);
+				BIT_CLEAR(MCUCR, ISC11);
+			}
+			else if(externalInterruptSense == DIO_EXTERNAL_INT_FALLING_EDGE)
+			{
+				BIT_CLEAR(MCUCR, ISC10);
+				BIT_SET(MCUCR, ISC11);
+			}
+			else if(externalInterruptSense == DIO_EXTERNAL_INT_RISING_EDGE)
+			{
+				BIT_SET(MCUCR, ISC10);
+				BIT_SET(MCUCR, ISC11);
+			}
+			else
+			{
+				return DIO_INVALID_EXTERNAL_INTERRUPT_SENSE;
+			}
+			
+			// Enable external interrupt 1 - INT1
+			BIT_SET(GICR, INT1);
+		
+			return DIO_NO_ERROR;
+		
+		case DIO_INT2:
+			/* Initialize External Interrupts for INT2 */
+		
+			// Enable global interrupts
+			sei();
+		
+			/* Set The External Interrupt Sense */
+			if(externalInterruptSense == DIO_EXTERNAL_INT_FALLING_EDGE)
+			{
+				BIT_CLEAR(MCUCSR, ISC2);
+			}
+			else if(externalInterruptSense == DIO_EXTERNAL_INT_RISING_EDGE)
+			{
+				BIT_SET(MCUCSR, ISC2);
+			}
+			else
+			{
+				return DIO_INVALID_EXTERNAL_INTERRUPT_SENSE;
+			}
+		
+			// Enable external interrupt 2 - INT2
+			BIT_SET(GICR, INT2);
+		
+			return DIO_NO_ERROR;
+		
+		default:
+			// External interrupt type error handling
+			return DIO_INVALID_INTERRUPT_TYPE;
+	}
+}
+
+EN_DIOErrorStatus_t DIO_disableExternalInterrupt(EN_DIOExternalInterrupt_t externalInterruptType)
+{
+	switch(externalInterruptType)
+	{
+		case DIO_INT0:
+			// Disable external interrupt 0 - INT0
+			BIT_CLEAR(GICR, INT0);
+		
+			return DIO_NO_ERROR;
+		
+		case DIO_INT1:
+			// Disable external interrupt 1 - INT1
+			BIT_CLEAR(GICR, INT1);
+		
+			return DIO_NO_ERROR;
+		
+		case DIO_INT2:
+			// Disable external interrupt 2 - INT2
+			BIT_CLEAR(GICR, INT2);
+		
+			return DIO_NO_ERROR;
+		
+		default:
+			// External interrupt type error handling
+			return DIO_INVALID_INTERRUPT_TYPE;
+	}
+}
+
+EN_DIOErrorStatus_t DIO_setExternalInterruptCallback(EN_DIOExternalInterrupt_t externalInterruptType, void(*callbackFunction)())
+{
+	switch(externalInterruptType)
+	{
+		case DIO_INT0:
+			INT0_CALLBACK_FUNCTION = callbackFunction;
+			
+			return DIO_NO_ERROR;
+			
+		case DIO_INT1:
+			INT1_CALLBACK_FUNCTION = callbackFunction;
+			
+			return DIO_NO_ERROR;
+			
+		case DIO_INT2:
+			INT2_CALLBACK_FUNCTION = callbackFunction;
+			
+			return DIO_NO_ERROR;
+		
+		default:
+			// External interrupt type error handling
+			return DIO_INVALID_INTERRUPT_TYPE;
+	}
+}
+
+ISR(EXT_INT_0_VECTOR)
+{
+	INT0_CALLBACK_FUNCTION();
+}
+
+ISR(EXT_INT_1_VECTOR)
+{
+	INT1_CALLBACK_FUNCTION();
+}
+
+ISR(EXT_INT_2_VECTOR)
+{
+	INT2_CALLBACK_FUNCTION();
+}
---
+++ Firmware/Automotive_Instrument_Cluster_HMI/ATMega32ALib/ATMega32A/MCAL/DIO/DIO.h
@@ -0,0 +1,171 @@
+/*
+ * DIO.h
+ *
+ * Created: 9/20/2022 9:09:45 PM
+ *  Author: MHamiid
+ */ 
+
+
+#ifndef DIO_H_
+#define DIO_H_
+
+#include <stdint.h>
+
+#define BIT_SET(byte, nbit)			(  (byte) |=  (1<<(nbit)) )
+#define BIT_CLEAR(byte, nbit)		(  (byte) &= ~(1<<(nbit)) )
+#define BIT_TOGGLE(byte, nbit)		(  (byte) ^=  (1<<(nbit)) )
+#define BIT_READ(byte, nbit)		( ((byte) &   (1<<(nbit))) >> nbit )  // Sets the read bit to be either 0 or 1
+
+
+typedef enum EN_DIOPort_t
+{
+	DIO_PORT_A,
+	DIO_PORT_B,
+	DIO_PORT_C,
+	DIO_PORT_D
+} EN_DIOPort_t;
+
+typedef enum EN_DIOPin_t
+{
+	DIO_PIN_0 = 0,
+	DIO_PIN_1,
+	DIO_PIN_2,
+	DIO_PIN_3,
+	DIO_PIN_4,
+	DIO_PIN_5,
+	DIO_PIN_6,
+	DIO_PIN_7,
+} EN_DIOPin_t;
+
+typedef enum EN_DIODirection_t
+{
+	DIO_DIRECTION_INPUT,
+	DIO_DIRECTION_OUTPUT
+} EN_DIODirection_t;
+
+/* Digital Values */
+typedef enum EN_DIODigitalValue_t
+{
+	DIO_LOW = 0,
+	DIO_HIGH = 1
+} EN_DIODigitalValue_t;
+
+typedef enum EN_DIOExternalInterrupt_t
+{
+	DIO_INT0,
+	DIO_INT1,
+	DIO_INT2
+} EN_DIOExternalInterrupt_t;
+
+typedef enum EN_DIOExternalInterruptSense_t
+{
+	DIO_EXTERNAL_INT_LOW_LEVEL,
+	DIO_EXTERNAL_INT_ANY_LOGICAL_CHANGE,
+	DIO_EXTERNAL_INT_FALLING_EDGE,
+	DIO_EXTERNAL_INT_RISING_EDGE,
+} EN_DIOExternalInterruptSense_t;
+
+
+typedef enum EN_DIOErrorStatus_t
+{
+	DIO_NO_ERROR,
+	DIO_INVALID_PORT,
+	DIO_INVALID_PIN_NUMBER,
+	DIO_INVALID_DIRECTION,
+	DIO_INVALID_DIGITAL_VALUE,
+	DIO_INVALID_INTERRUPT_TYPE,
+	DIO_INVALID_EXTERNAL_INTERRUPT_SENSE
+} EN_DIOErrorStatus_t;
+
+
+
+/**
+ * @brief Initialize a DIO Pin in the specified Port by setting its direction
+ * 
+ * @param port							The Port where the Pin is located
+ * @param pinNumber						The Pin Number in the Port
+ * @param direction						Pin Direction
+ *
+ * @return DIO_NO_ERROR					Initialization was successful
+ * @return DIO_INVALID_PIN_NUMBER		Initialization failed, invalid pin number
+ * @return DIO_INVALID_DIRECTION		Initialization failed, invalid direction
+ * @return DIO_INVALID_PORT				Initialization failed, invalid port
+ */
+EN_DIOErrorStatus_t DIO_init(EN_DIOPort_t port, EN_DIOPin_t pinNumber, EN_DIODirection_t direction);
+
+/**
+ * @brief Write a digital output value to the DIO Pin in the specified Port
+ * 
+ * @param port							The port where the pin is located
+ * @param pinNumber						The pin number in the port
+ * @param digitalValue					Digital output value
+ *
+ * @return DIO_NO_ERROR					Digital Write was successful
+ * @return DIO_INVALID_PIN_NUMBER		Digital Write failed, invalid pin number
+ * @return DIO_INVALID_DIGITAL_VALUE	Digital Write failed, invalid digital value
+ * @return DIO_INVALID_PORT				Digital Write failed, invalid port
+ */
+EN_DIOErrorStatus_t DIO_write(EN_DIOPort_t port, EN_DIOPin_t pinNumber, EN_DIODigitalValue_t digitalValue);
+
+/**
+ * @brief Toggle the current digital value of a DIO Pin in the specified Port
+ * 
+ * @param port							The port where the pin is located
+ * @param pinNumber						The pin number in the port
+ *
+ * @return DIO_NO_ERROR					Digital Toggle was successful
+ * @return DIO_INVALID_PIN_NUMBER		Digital Toggle failed, invalid pin number
+ * @return DIO_INVALID_PORT				Digital Toggle failed, invalid port
+ */
+EN_DIOErrorStatus_t DIO_toggle(EN_DIOPort_t port, EN_DIOPin_t pinNumber);
+
+/**
+ * @brief Read the current digital value of a DIO Pin in the specified Port
+ * 
+ * @param port							The port where the pin is located
+ * @param pinNumber						The pin number in the port
+ * @param retDigitalValue				The read digital value
+ *
+ * @return DIO_NO_ERROR					Digital Read was successful
+ * @return DIO_INVALID_PIN_NUMBER		Digital Read failed, invalid pin number
+ * @return DIO_INVALID_PORT				Digital Read failed, invalid port
+ */
+EN_DIOErrorStatus_t DIO_read(EN_DIOPort_t port, EN_DIOPin_t pinNumber, EN_DIODigitalValue_t* retDigitalValue);
+
+/**
+ * @brief Enable External Interrupt for the specified DIO external interrupt type with the specified interrupt sense
+ * 
+ *	Note that (DIO_INT2) has only two possible interrupt senses [ DIO_EXTERNAL_INT_FALLING_EDGE, DIO_EXTERNAL_INT_RISING_EDGE ]
+ *
+ * @param externalInterruptType						The External Interrupt to be enabled
+ * @param externalInterruptSense					Interrupt sense type
+ *
+ * @return DIO_NO_ERROR								External Interrupt enable was successful
+ * @return DIO_INVALID_EXTERNAL_INTERRUPT_SENSE		External Interrupt enable failed, invalid external interrupt sense
+ * @return DIO_INVALID_INTERRUPT_TYPE				External Interrupt enable failed, invalid external interrupt type
+ */
+EN_DIOErrorStatus_t DIO_enableExternalInterrupt(EN_DIOExternalInterrupt_t externalInterruptType, EN_DIOExternalInterruptSense_t externalInterruptSense);
+
+/**
+ * @brief Disable External Interrupt for the specified DIO external interrupt type
+ * 
+ * @param externalInterruptType					The External Interrupt to be disabled
+ *
+ * @return DIO_NO_ERROR							External Interrupt disable was successful
+ * @return DIO_INVALID_INTERRUPT_TYPE			External Interrupt disable failed, invalid external interrupt type
+ */
+EN_DIOErrorStatus_t DIO_disableExternalInterrupt(EN_DIOExternalInterrupt_t externalInterruptType);
+
+/**
+ * @brief Set a callback function to be called inside the ISR for the specified DIO external interrupt type
+ * 
+ * @param externalInterruptType			The External Interrupt to set the callback function
+ * @param callbackFunction				void function will be called when the ISR for the External Interrupt is called
+ *
+ * @return DIO_NO_ERROR					Setting a callback function for the External Interrupt was successful
+ * @return DIO_INVALID_INTERRUPT_TYPE	Setting a callback function for the External Interrupt failed, invalid external interrupt type
+ */
+EN_DIOErrorStatus_t DIO_setExternalInterruptCallback(EN_DIOExternalInterrupt_t externalInterruptType, void(*callbackFunction)());
+
+
+#endif /* DIO_H_ */
