a6c77d5c0dbffc6bcdb570b7db739997059e3e63
MHamiid <mmh699@outlook.com>
2022-09-22 18:31:24 +0200

    [Lib] Add bit definitions for Timer0 registers

--- Firmware/Automotive_Instrument_Cluster_HMI/ATMega32ALib/ATMega32A/Utilities/registers.h
+++ Firmware/Automotive_Instrument_Cluster_HMI/ATMega32ALib/ATMega32A/Utilities/registers.h
@@ -18,22 +18,22 @@
 /* DIO Registers                                                        */
 /************************************************************************/
 
-// PORTA registers
+/* PORTA Registers */
 #define PORTA	(*((volatile uint8_t*)0x3B))
 #define DDRA	(*((volatile uint8_t*)0x3A))
 #define PINA	(*((volatile uint8_t*)0x39))
 
-// PORTB registers
+/* PORTB Registers */
 #define PORTB	(*((volatile uint8_t*)0x38))
 #define DDRB	(*((volatile uint8_t*)0x37))
 #define PINB	(*((volatile uint8_t*)0x36))
 
-// PORTC registers
+/* PORTC Registers */
 #define PORTC	(*((volatile uint8_t*)0x35))
 #define DDRC	(*((volatile uint8_t*)0x34))
 #define PINC	(*((volatile uint8_t*)0x33))
 
-// PORTD registers
+/* PORTD Registers */
 #define PORTD	(*((volatile uint8_t*)0x32))
 #define DDRD	(*((volatile uint8_t*)0x31))
 #define PIND	(*((volatile uint8_t*)0x30))
@@ -42,10 +42,20 @@
 /* Timer Registers                                                      */
 /************************************************************************/
 
-// Timer 0
-#define TCCR0	(*((volatile uint8_t*)0x53))
+/* Timer 0 */
 #define TCNT0	(*((volatile uint8_t*)0x52))
+#define OCR0	(*((volatile uint8_t*)0x5C))
 #define TIFR	(*((volatile uint8_t*)0x58))
+#define TCCR0	(*((volatile uint8_t*)0x53))
+/* TCCR0 Bits */
+#define CS00    0
+#define CS01    1
+#define CS02    2
+#define WGM01   3
+#define COM00   4
+#define COM01   5
+#define WGM00   6
+#define FOC0    7
 
 /************************************************************************/
 /* External Interrupt Registers                                         */
