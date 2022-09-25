462598e34cb3a8eb4aeea787c442a3f4690644c9
MHamiid <mmh699@outlook.com>
2022-09-25 18:40:07 +0200

    [NodeOne] Add Application files
    Call application_init() and application_loop() in main

---
+++ Firmware/Automotive_Instrument_Cluster_HMI/NodeOne/Application/Application.c
@@ -0,0 +1,20 @@
+/*
+ * Application.c
+ *
+ * Created: 9/25/2022 6:26:59 PM
+ *  Author: MHamiid
+ */ 
+
+
+#include "Application.h"
+
+
+void application_init()
+{
+	
+}
+
+void application_loop()
+{
+	
+}
---
+++ Firmware/Automotive_Instrument_Cluster_HMI/NodeOne/Application/Application.h
@@ -0,0 +1,28 @@
+/*
+ * Application.h
+ *
+ * Created: 9/25/2022 6:26:50 PM
+ *  Author: MHamiid
+ */ 
+
+
+#ifndef APPLICATION_H_
+#define APPLICATION_H_
+
+
+/**
+ * @brief Called only once on start
+ *
+ * @return void
+ */
+void application_init();
+
+/**
+ * @brief Constantly called in a loop
+ *
+ * @return void
+ */
+void application_loop();
+
+
+#endif /* APPLICATION_H_ */
