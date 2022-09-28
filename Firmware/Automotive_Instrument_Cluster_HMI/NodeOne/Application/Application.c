6e82148c00c1f65df191fc3e3985265312881d7d
MHamiid <mmh699@outlook.com>
2022-09-28 19:34:03 +0200

    [NodeOne] Add initial TWI slave setup

--- Firmware/Automotive_Instrument_Cluster_HMI/NodeOne/Application/Application.c
+++ Firmware/Automotive_Instrument_Cluster_HMI/NodeOne/Application/Application.c
@@ -7,6 +7,7 @@
 
 
 #include "Application.h"
+#include <ATMega32A/MCAL/TWI/TWI.h>
 #include <ATMega32A/ECUAL/Motor/Motor.h>
 #include <ATMega32A/ECUAL/Accelerometer/Accelerometer.h>
 #include <ATMega32A/ECUAL/LM35/LM35.h>
@@ -14,8 +15,22 @@
 static float gs_accelerometerValue = 0.0f;
 static float gs_temperatureValue = 0.0f;
 
+/* Handle TWI interrupts */
+static void TWIInterruptCallback()
+{
+	// Get the current TWI status
+	EN_TWI_EVENT_STATUS_t TWI_status = TWI_getStatus();
+	
+	/* Handle TWI slave logic based on the current TWI_status */
+}
+
 void application_init()
 {
+	// Initialize TWI in slave mode with own slave address 0xA0
+	TWI_slave_init(0xA0);
+	// Set TWI interrupt callback function
+	TWI_setInterruptCallback(TWIInterruptCallback);
+	
 	motor_init(ADC_CHANNEL_0, PWM_TIMER2);
 	accelerometer_init(ADC_CHANNEL_1);
 	LM35_init(ADC_CHANNEL_2);
@@ -23,6 +38,9 @@ void application_init()
 
 void application_loop()
 {
+	// Start listening for own slave address on the TWI bus and handle TWI status in the interrupt callback function
+	TWI_slave_listen(true);
+	
 	motor_start(ADC_CHANNEL_0, PWM_TIMER2);
 	gs_accelerometerValue = accelerometer_read(ADC_CHANNEL_1);
 	gs_temperatureValue = LM35_read(ADC_CHANNEL_2);
