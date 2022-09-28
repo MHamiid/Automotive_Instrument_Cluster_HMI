653b2b24d7a93d9e71c9399b112a7a626e46604d
MHamiid <mmh699@outlook.com>
2022-09-28 17:43:42 +0200

    [NodeOne] Add initial application logic for the connected external devices

--- Firmware/Automotive_Instrument_Cluster_HMI/NodeOne/Application/Application.c
+++ Firmware/Automotive_Instrument_Cluster_HMI/NodeOne/Application/Application.c
@@ -7,14 +7,23 @@
 
 
 #include "Application.h"
+#include <ATMega32A/ECUAL/Motor/Motor.h>
+#include <ATMega32A/ECUAL/Accelerometer/Accelerometer.h>
+#include <ATMega32A/ECUAL/LM35/LM35.h>
 
+static float gs_accelerometerValue = 0.0f;
+static float gs_temperatureValue = 0.0f;
 
 void application_init()
 {
-	
+	motor_init(ADC_CHANNEL_0, PWM_TIMER2);
+	accelerometer_init(ADC_CHANNEL_1);
+	LM35_init(ADC_CHANNEL_2);
 }
 
 void application_loop()
 {
-	
+	motor_start(ADC_CHANNEL_0, PWM_TIMER2);
+	gs_accelerometerValue = accelerometer_read(ADC_CHANNEL_1);
+	gs_temperatureValue = LM35_read(ADC_CHANNEL_2);
 }
