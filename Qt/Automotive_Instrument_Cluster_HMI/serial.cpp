8821dd561dc200782355beedbfd3e494078c4275
MHamiid <mmh699@outlook.com>
2022-10-15 00:42:51 +0200

    [Qt] Add read and parse device data frame in Serial class

--- Qt/Automotive_Instrument_Cluster_HMI/serial.cpp
+++ Qt/Automotive_Instrument_Cluster_HMI/serial.cpp
@@ -1,6 +1,85 @@
 #include "serial.h"
+#include <QDebug>
+
+#define DEVICE_DATA_FRAME_START_DELIMITER '|'
+#define DEVICE_DATA_FRAME_END_DELIMITER '\r'
 
 Serial::Serial(QObject* parent) : QSerialPort(parent)
 {
+    // Create signal slot connection to make readAndParseDeviceData() called whenever there is a new data ready to be read
+    QObject::connect(this, &QIODevice::readyRead, this, &Serial::readAndParseDeviceData);
+}
+
+void Serial::readAndParseDeviceData()
+{
+
+    if(startOfDeviceDataFrameReceieved)
+    {
+        // Read a byte from the received device data frame into the receivedDataBuffer at the receivedDataBufferIndex position
+        if(read((receivedDataBuffer + receivedDataBufferIndex), 1))
+        {
+            /*
+             * If reached the end of the frame, then parse the device data and set it up in the correct
+             * device output format to be called in [SIGNAL] deviceDataAvailable(deviceOutput)
+            */
+            if(receivedDataBuffer[receivedDataBufferIndex] == DEVICE_DATA_FRAME_END_DELIMITER)
+            {
+                /* Process the received device data frame in the buffer */
+                QList<QVariant> deviceDataOut(2);
+                // First byte in the device data frame is the device address
+                uint8_t deviceAddress = receivedDataBuffer[0];
+
+                switch (deviceAddress)
+                {
+                    case DEVICE_INTERNAL_ADDRESS_MOTOR:
+                        deviceDataOut[0] = DEVICE_INTERNAL_ADDRESS_MOTOR;
+                        deviceDataOut[1] = receivedDataBuffer[1];
+                        break;
+                    case DEVICE_INTERNAL_ADDRESS_ACCELEROMETER:
+                        deviceDataOut[0] = DEVICE_INTERNAL_ADDRESS_ACCELEROMETER;
+
+                        // Convert the received four data bytes back to float
+                        float accelerometerValue;
+                        memcpy (&accelerometerValue, (receivedDataBuffer + 1), 4);
+                        deviceDataOut[1] = accelerometerValue;
+                        break;
+                    case DEVICE_INTERNAL_ADDRESS_LM35:
+                        deviceDataOut[0] = DEVICE_INTERNAL_ADDRESS_LM35;
+
+                        // Convert the received four data bytes back to float
+                        float tempratureValue;
+                        memcpy (&tempratureValue, (receivedDataBuffer + 1), 4);
+                        deviceDataOut[1] = tempratureValue;
+                        break;
+                    default:
+                        // Error Handing: Unknown device address
+                        break;
+                }
+
+                // Emit device data available signal with the device data
+                 emit deviceDataAvailable(deviceDataOut);
+
+                // Reset the start of frame received status
+                startOfDeviceDataFrameReceieved = false;
+                // Reset the buffer index
+                receivedDataBufferIndex = 0;
+            }
+            /*
+             * If DEVICE_DATA_FRAME_END_DELIMITER hasn't been reached yet, then move to the next index in the buffer to write the next byte
+            */
+            else
+            {
+                receivedDataBufferIndex++;
+            }
+        }
+    }
+    /*
+     * Check if the received byte is the DEVICE_DATA_FRAME_START_DELIMITER, so we can start
+     * reading the device data into the buffer on the next readAndParseDeviceData() call
+    */
+    else if(read(receivedDataBuffer, 1) && receivedDataBuffer[0] == DEVICE_DATA_FRAME_START_DELIMITER)
+    {
+        startOfDeviceDataFrameReceieved = true;
+    }
 
 }
--- Qt/Automotive_Instrument_Cluster_HMI/serial.h
+++ Qt/Automotive_Instrument_Cluster_HMI/serial.h
@@ -2,6 +2,8 @@
 #define SERIAL_H
 
 #include <QSerialPort>
+#include <QVariant>
+#include <QList>
 
 class Serial : public QSerialPort
 {
@@ -10,12 +12,53 @@ class Serial : public QSerialPort
     Q_PROPERTY(QIODeviceBase::OpenMode openMode READ openMode WRITE open NOTIFY openModeChanged)
 
 public:
+    enum DeviceInternalAddress
+    {
+        DEVICE_INTERNAL_ADDRESS_MOTOR			=     0x01,
+        DEVICE_INTERNAL_ADDRESS_ACCELEROMETER	=     0x02,
+        DEVICE_INTERNAL_ADDRESS_LM35			=     0x03
+    };
+    // Add Q_ENUM to make it callable in the QML side
+    Q_ENUM(DeviceInternalAddress)
+
     Serial(QObject* parent = nullptr);
 
+    /*
+     * @brief [SLOT] Read and parse device date frame from the serial port
+     *
+     * This slot is connected to [SIGNAL] QIODevice::readyRead()
+     * And emits [SIGNAL] deviceDataAvailable() when a complete device data frame is received
+     *
+     * @return void
+    */
+    void readAndParseDeviceData();
+
 Q_SIGNALS:
-    /* These signals are currently not emitted/used, they are mostly here to avoid Qt warnings */
+    /* These signals (portNameChanged, openModeChanged) are currently not emitted/used, they are mostly here to avoid Qt warnings */
     void portNameChanged(QString portName);
     void openModeChanged(QIODeviceBase::OpenMode openMode);
+
+    /*
+     * @brief [SIGNAL] Emitted/Called whenever a complete device data frame is read
+     *
+     * @param deviceData { DeviceAddress, DeviceData }. Where DeviceData can be a byte or a float depending on which DeviceAddress is it
+     *
+     * @return void
+    */
+    void deviceDataAvailable(QList<QVariant> deviceData);
+
+private:
+    /*
+     * Buffer size = Max device data frame size:
+     *          1 byte for the device address, 4 bytes max for devices's data(as float is 4 bytes) ,or 1 byte, and 1 byte for the MESSAGE_END_DELIMITER
+     *
+     * Note that DEVICE_DATA_FRAME_START_DELIMITER gets always overridden in the buffer by the first
+     * byte of the device data, as such there is no space allocated in the buffer for it.
+    */
+    char receivedDataBuffer[6] = { 0 };
+    size_t receivedDataBufferIndex = 0;
+    // Indication when true that data is being written to receivedDataBuffer
+    bool startOfDeviceDataFrameReceieved = false;
 };
 
 #endif // SERIAL_H
