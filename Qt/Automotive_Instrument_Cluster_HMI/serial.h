14ca8feabe8a90dea78b8180ca2075ff88c09c57
MHamiid <mmh699@outlook.com>
2022-10-14 16:58:06 +0200

    [Qt] Add NOTIFY signals to the properties in Serial class
    Add the NOTIFY signals even they are not emitted or used, but just to avoid Qt warnings

--- Qt/Automotive_Instrument_Cluster_HMI/serial.h
+++ Qt/Automotive_Instrument_Cluster_HMI/serial.h
@@ -6,11 +6,16 @@
 class Serial : public QSerialPort
 {
     Q_OBJECT
-    Q_PROPERTY(QString portName READ portName WRITE setPortName)
-    Q_PROPERTY(OpenMode openMode READ openMode WRITE open)
+    Q_PROPERTY(QString portName READ portName WRITE setPortName NOTIFY portNameChanged)
+    Q_PROPERTY(QIODeviceBase::OpenMode openMode READ openMode WRITE open NOTIFY openModeChanged)
 
 public:
     Serial(QObject* parent = nullptr);
+
+Q_SIGNALS:
+    /* These signals are currently not emitted/used, they are mostly here to avoid Qt warnings */
+    void portNameChanged(QString portName);
+    void openModeChanged(QIODeviceBase::OpenMode openMode);
 };
 
 #endif // SERIAL_H
