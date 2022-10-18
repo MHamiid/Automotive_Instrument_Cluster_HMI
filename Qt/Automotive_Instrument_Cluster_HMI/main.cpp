642d3c1c7679d5673d70443ac0881835cf637697
MHamiid <mmh699@outlook.com>
2022-10-18 06:39:01 +0200

    [Qt] Export Serial class to the QML side

--- Qt/Automotive_Instrument_Cluster_HMI/main.cpp
+++ Qt/Automotive_Instrument_Cluster_HMI/main.cpp
@@ -1,6 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include "serial.h"
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +8,9 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
     QGuiApplication app(argc, argv);
+
+    // Export Serial class to the QML side
+    qmlRegisterType<Serial>("Serial", 1, 0, "Serial");
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
